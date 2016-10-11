36f652368baa63e7b2c30f70c64cf84d0991b9cb
Karlo <808khz@gmail.com>
2016-10-11 17:14:04 -0500

    subí Tres Clases : FONDO NIVEL PEREZA TEXTO

---
+++ core/src/itesm/mx/Texto.java
@@ -0,0 +1,48 @@
+package itesm.mx;
+
+
+import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.graphics.Color;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+
+ * Created by rmroman on 05/02/16.
+
+ */
+
+public class Texto
+
+{
+
+    private BitmapFont font;
+
+
+
+    public Texto() {
+
+        font = new BitmapFont(Gdx.files.internal("tipoMicrosoft.fnt"));
+
+    }
+
+
+
+    public void mostrarMensaje(SpriteBatch batch, String mensaje, float x, float y) {
+
+        GlyphLayout glyp = new GlyphLayout();
+
+        glyp.setText(font, mensaje);
+
+        float anchoTexto = glyp.width;
+
+        font.draw(batch,glyp,x-anchoTexto/2,y);
+
+    }
+
+}
