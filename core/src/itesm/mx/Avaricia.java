f5811ac8479008eb1d31468717c2d64508387e53
Marina <A01375051@itesm.mx>
2016-10-12 19:33:18 -0500

    Modificando Avaricia_2

--- core/src/itesm/mx/Avaricia.java
+++ core/src/itesm/mx/Avaricia.java
@@ -40,7 +40,7 @@ public class Avaricia implements Screen, InputProcessor {
     int vidas;
     int almas;
 
-    //variables constantes de ancho y alto de la pamtalla
+    //variables constantes de ancho y alto de la pantalla
     private final float ancho = 1280;
     private final float alto = 720;
 
