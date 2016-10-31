e7b3811968f9995644a2d66ead7909a602393ef2
drkalavera <808khz@gmail.com>
2016-10-31 17:57:12 -0600

    Se implementó la lectura y el muestreo de scores

--- core/src/itesm/mx/Score.java
+++ core/src/itesm/mx/Score.java
@@ -1,41 +1,41 @@
 package itesm.mx;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import java.io.*;
 import java.util.*;
 
-//TODO:hacer textos y mostrarlos
-//TODO: guardar nuevos scores
+
+//TODO:Acomodar el texto
 
 /**
  * Created by Daniel Riv on 16/09/2016.
+ * Modified by Karlo on 10/31/16
  */
-public class Score implements Screen {
+public class Score implements Screen, InputProcessor {
+
+    FileHandle scrs = Gdx.files.internal("scores.txt");
+
     //se inicializa con una variable Tipo juego para poder pasar a otra escena
-    private final itesm.mx.juego juego;
+    private final juego juego;
 
     //variables constantes de ancho y alto de la pamtalla
     private final int ancho = 1280;
     private final int alto = 720;
 
-    //escena para la pantalla
-    private Stage escena;
+    //SpriteBatch
+    private SpriteBatch batch;
+    private Fondo fondo;
 
     //textura para la imagen de fondo
     private Texture texturaFondo;
@@ -46,6 +46,12 @@ public class Score implements Screen {
     //Para los scores
     private ArrayList<String> nombres = new ArrayList<String>(5);
     private ArrayList<Integer> scores = new ArrayList<Integer>(5);
+    private Texto scr1;
+    private Texto scr2;
+    private Texto scr3;
+    private Texto scr4;
+    private Texto scr5;
+
 
     //administra la carga de assets
     private final AssetManager assetManager = new AssetManager();
@@ -57,10 +63,6 @@ public class Score implements Screen {
     //musica
     private final Music musica;
 
-    Scanner scan = null;
-    String titulo, artista, genero;
-    double precio;
-
     //constructor
     public Score(itesm.mx.juego juego, Music musica){
         this.juego =  juego;
@@ -70,107 +72,82 @@ public class Score implements Screen {
 
     //
     private void cargarScores() {
-        try {
-            scan= new Scanner(new FileReader("scores.txt"));
-            while(scan.hasNext())
+        String alle = scrs.readString();
+        StringBuilder sb = new StringBuilder(50);
+        char[] arrCar = alle.toCharArray();
+
+        for(int i=0; i< arrCar.length; i++)
+        {
+            if(arrCar[i]=='\n')
             {
-                String [] lines = scan.nextLine().toString().split("	");
-                nombres.add(lines[0]);
-                scores.add(Integer.parseInt(lines[1]));
+                scores.add(Integer.parseInt(sb.toString()));
+                sb = new StringBuilder();
             }
-        }catch(FileNotFoundException e) {
-            System.err.println(e);
-        } catch(InputMismatchException e) {
-            System.err.println(e);
-        } catch (java.util.NoSuchElementException e) {
-            System.err.println(e);
-        }
-        finally {
-            scan.close();
+            else if (arrCar[i]==' ')
+            {
+                nombres.add(sb.toString());
+                sb = new StringBuilder();
+            }
+            if(Character.isDigit(arrCar[i])||Character.isLetter(arrCar[i]))
+            {
+                sb.append(arrCar[i]);
+            }
         }
     }
 
     @Override
     public void show() {
-        //equivalente a create o a start, se ejecuta solo al cargar la pantalla
+        cargarTexturas();
+        inicializarCamara();
+        crearEscena();
+        Gdx.input.setInputProcessor(this);
+        scr1 = new Texto();
+        scr2 = new Texto();
+        scr3 = new Texto();
+        scr4 = new Texto();
+        scr5 = new Texto();
+    }
 
+    private void crearEscena()
+    {
+        batch = new SpriteBatch();
+        fondo = new Fondo(texturaFondo);
+    }
+
+    private void inicializarCamara()
+    {
         camara = new OrthographicCamera(ancho,alto);
         camara.position.set(ancho/2,alto/2,0);
-
         camara.update();
-        //vista = new StretchViewport(ancho,alto,camara);
-        vista = new FitViewport(ancho,alto,camara);
-
-        escena = new Stage();
-
-        //habilitar el manejo de eventos
-        Gdx.input.setInputProcessor(escena);
-        cargarTexturas();
+        vista = new StretchViewport(ancho,alto,camara);
     }
 
-    public void cargarTexturas(){
-
-
-
-        //textura de fondo
-        assetManager.load("Scores.png",Texture.class);
-
-        //texturas de botones
-        assetManager.load("back.png",Texture.class);
-
-        //se bloquea hasta cargar los recursos
-        assetManager.finishLoading();//bloquea hasta que se carguen las imgenes
-        //cuando termina, leemos las texturas
-        texturaFondo = assetManager.get("Scores.png");
-        texturaBtnBack = assetManager.get("back.png");
-
-        anadirTexturas();
-    }
-
-    public void anadirTexturas(){
-
-        //para el fondo
-        Image imgFondo = new Image(texturaFondo);
-        //Escalar
-        float escalaX = ancho / imgFondo.getWidth();
-        float escalaY = alto / imgFondo.getHeight();
-        imgFondo.setScale(escalaX,escalaY);
-        escena.addActor(imgFondo);
-
-        //botones
-        //btn back
-        TextureRegionDrawable trBtnBack = new TextureRegionDrawable(new TextureRegion(texturaBtnBack));
-        ImageButton btnBack = new ImageButton(trBtnBack);
-        btnBack.setPosition(btnBack.getWidth()/6, alto*.2f);
-        escena.addActor(btnBack);
-
-
-
-        //registrar listener para registarar evento del boton
-        btnBack.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.app.log("clicked", "TAP sobre el boton de regresar");
-                juego.setScreen(new MenuPrincipal(juego,musica));
-            }
-        });
-
+    public void cargarTexturas()
+    {
+        texturaFondo = new Texture("Scores.png");
+        texturaBtnBack = new Texture("back.png");
     }
 
     @Override
     public void render(float delta) {
-    //es el update
-        //pantalla blanca
-        Gdx.gl.glClearColor(0,0,0,1);
-        //borra la pantalla completamente
+        Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(camara.combined);
+        batch.begin();
+        //aki se dibujan los elementos
+        fondo.draw(batch);
 
-        escena.setViewport(vista);
-        escena.draw();
+        scr1.mostrarMensaje(batch, scores.get(0)+" "+nombres.get(0), ancho/2, 720);
+        scr2.mostrarMensaje(batch, scores.get(1)+" "+nombres.get(1), ancho/2, 620);
+        scr3.mostrarMensaje(batch, scores.get(2)+" "+nombres.get(2), ancho/2, 520);
+        scr4.mostrarMensaje(batch, scores.get(3)+" "+nombres.get(3), ancho/2, 420);
+        scr5.mostrarMensaje(batch, scores.get(4)+" "+nombres.get(4), ancho/2, 320);
+        batch.end();
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height)
+    {
         vista.update(width, height);
     }
 
@@ -192,9 +169,52 @@ public class Score implements Screen {
     @Override
     public void dispose() {
         //liberar los recursos utilizados en la memoria
-
         texturaBtnBack.dispose();
         texturaFondo.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector3 v = new Vector3(screenX,screenY,0);
+        camara.unproject(v);
+        float x = v.x;
+        float y = v.y;
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
 
