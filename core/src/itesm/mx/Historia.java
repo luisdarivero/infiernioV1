0817da370de83f785f4b26d3ed463dfc12fa2d2c
drkalavera <808khz@gmail.com>
2016-11-28 12:12:08 -0600

    h

--- core/src/itesm/mx/Historia.java
+++ core/src/itesm/mx/Historia.java
@@ -155,7 +155,7 @@ public class Historia implements Screen, InputProcessor
     }
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.setProjectionMatrix(camara.combined);
         boolean[] k = {false, false,false,false};
